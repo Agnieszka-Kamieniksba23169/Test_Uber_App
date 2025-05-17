813d8e66e3758a2fe7be39ffab9b7cb03010dc27
Agnieszka-Kamieniksba23169 <sba23169@student.cct.ie>
2025-05-17 15:27:50 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+from io import StringIO
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set visual style
+sns.set(style="whitegrid")
